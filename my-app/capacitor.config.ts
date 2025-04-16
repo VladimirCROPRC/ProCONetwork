2f2d87413bdcee721186945618cc294b11d380c6
vladimircroprc <vladimir.carlan@proconect.ro>
2025-04-16 23:54:33 +0300

    Ready for Codemagic build

---
+++ my-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proconect.network',
+  appName: 'VDFSites',
+  webDir: 'build'
+};
+
+export default config;
